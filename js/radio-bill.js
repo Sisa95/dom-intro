17266153a6ab300af95f6b47227930e996a10ccf
sisa95 <ponyesisa@gmail.com>
2021-04-19 16:52:40 +0200

    working radio button function

--- js/radio-bill.js
+++ js/radio-bill.js
@@ -10,3 +10,34 @@
 // * add the appropriate value to the running total
 // * add nothing for invalid values that is not 'call' or 'sms'.
 // * display the latest total on the screen
+
+
+/*function increment(incrementor, target){
+    var value = parseInt(document.getElementById(target).value);
+    value+=incrementor;
+    document.getElementById(target).value = value;
+} */
+
+
+
+
+function totalPhoneBill() {
+    var callCost = 2.75;
+    var smsCost = 0.65;
+    var totalBill;
+    var callTotal = 0;
+
+    var checkedRadioBtn = document.querySelector("input[name='billItemType']:checked");
+if (checkedRadioBtn){
+    var billItemType = checkedRadioBtn.value
+    // billItemType will be 'call' or 'sms'alert();
+    if(billItemType == "call"){
+        alert()
+        var bill = parseInt(document.querySelector(".billString").value);
+        bill += callCost   
+        var billCost = document.querySelector(".radioBillAddBtn").innerHTML = bill
+
+    }
+}
+   
+}
--- js/text-bill.js
+++ js/text-bill.js
@@ -10,3 +10,59 @@
 // * add the appropriate value to the running total
 // * add nothing for invalid values that is not 'call' or 'sms'.
 // * display the latest total on the screen
+
+
+function totalPhoneBill () {
+    var bill = document.querySelector(".billString").value;
+
+    var callCount = 0;
+  	var smsCount = 0;
+    var callCost = 2.75;
+    var smsCost = 0.65;
+    var totalBill;
+    var str = bill.replace(/ /g,'')
+  	var input = str.split(',');
+ 
+    for(var i = 0; i < input.length; i++) {
+        if(input[i] === "call") {
+            callCount++;
+        } else if(input[i] === "sms"){
+        	smsCount++;
+        }
+    }
+    totalBill = (smsCount * smsCost) + (callCount * callCost)
+   
+    if(totalBill >=20 && totalBill < 30){
+        document.querySelector(".billTotal").style.color = "orange";
+        document.querySelector(".billTotal").innerHTML = totalBill.toFixed(2);
+    } else if(totalBill >=30){
+        document.querySelector(".billTotal").style.color = "red";
+        document.querySelector(".billTotal").innerHTML = totalBill.toFixed(2);
+    } else {
+    document.querySelector(".billTotal").innerHTML = totalBill.toFixed(2);
+    }
+}
+
+setInterval(totalPhoneBill, 1000);
+
+ /* 
+        ******TODO*****
+    add keydown event listner on key down C = write call to text area S = write
+    sms to text area        
+    add range slider 
+    
+    
+    code example
+    
+    <textarea id="myTextarea">
+342 Alvin Road
+Ducksburg</textarea>
+
+<p>Click the button to change the contents of the text area.</p>
+
+<button type="button" onclick="myFunction()">Try it</button>
+
+<script>
+function myFunction() {
+  document.getElementById("myTextarea").value = "Fifth Avenue, New York City";
+}*/
