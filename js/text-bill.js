1a361a2476755c79bb8d64adbe84cc11ff93a671
sisa95 <ponyesisa@gmail.com>
2021-04-21 14:22:47 +0200

    incomplete functions

--- js/text-bill.js
+++ js/text-bill.js
@@ -1,19 +1,21 @@
 // get a reference to the textbox where the bill type is to be entered
 
 //get a reference to the add button
-
+var addButton = document.querySelector(".addToBillBtn")
 //create a variable that will keep track of the total bill
-
-//add an event listener for when the add button is pressed
+var callTotalOne = document.querySelector(".callTotalOne")
+var smsTotalOne = document.querySelector(".smsTotalOne")
+var totalOne = document.querySelector(".totalOne")
+//add an event listener for when the add button is pressed 
 
 //in the event listener check if the value in the bill type textbox is 'sms' or 'call'
+
+
 // * add the appropriate value to the running total
 // * add nothing for invalid values that is not 'call' or 'sms'.
 // * display the latest total on the screen
-
-
-function totalPhoneBill () {
-    var bill = document.querySelector(".billString").value;
+function textBillTotal() {
+    var bill = document.querySelector(".billTypeText").value
 
     var callCount = 0;
   	var smsCount = 0;
@@ -22,47 +24,30 @@ function totalPhoneBill () {
     var totalBill;
     var str = bill.replace(/ /g,'')
   	var input = str.split(',');
- 
-    for(var i = 0; i < input.length; i++) {
-        if(input[i] === "call") {
-            callCount++;
+
+     
+        if(bill === "call") {
+            callCount += callCost
+            
+            
         } else if(input[i] === "sms"){
         	smsCount++;
         }
-    }
+    
+  
     totalBill = (smsCount * smsCost) + (callCount * callCost)
    
     if(totalBill >=20 && totalBill < 30){
-        document.querySelector(".billTotal").style.color = "orange";
-        document.querySelector(".billTotal").innerHTML = totalBill.toFixed(2);
+        document.querySelector(".callTotalOne").style.color = "orange";
+        document.querySelector(".callTotalOne").innerHTML = callCount.toFixed(2);
     } else if(totalBill >=30){
-        document.querySelector(".billTotal").style.color = "red";
-        document.querySelector(".billTotal").innerHTML = totalBill.toFixed(2);
-    } else {
-    document.querySelector(".billTotal").innerHTML = totalBill.toFixed(2);
+        document.querySelector(".callTotalOne").style.color = "red";
+        document.querySelector(".callTotalOne").innerHTML = callCount.toFixed(2);
+    } else if(totalBill < 20){
+        document.querySelector(".callTotalOne").style.color = "black";
+        document.querySelector(".callTotalOne").innerHTML = callCount.toFixed(2);
     }
+    document.querySelector(".callTotalOne").innerHTML = callCount.toFixed(2);
 }
 
-setInterval(totalPhoneBill, 1000);
-
- /* 
-        ******TODO*****
-    add keydown event listner on key down C = write call to text area S = write
-    sms to text area        
-    add range slider 
-    
-    
-    code example
-    
-    <textarea id="myTextarea">
-342 Alvin Road
-Ducksburg</textarea>
-
-<p>Click the button to change the contents of the text area.</p>
-
-<button type="button" onclick="myFunction()">Try it</button>
-
-<script>
-function myFunction() {
-  document.getElementById("myTextarea").value = "Fifth Avenue, New York City";
-}*/
+addButton.addEventListener("click", textBillTotal)
